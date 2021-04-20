040ff3a5db5e45b89fcd526b337645d856ca6e73
zero <1580359334@qq.com>
2021-04-20 16:25:27 +0800

    登入功能实现、登入拦截器、展示员工列表、拆分页面

--- src/main/java/com/huang/config/MyMvcConfig.java
+++ src/main/java/com/huang/config/MyMvcConfig.java
@@ -3,7 +3,7 @@ package com.huang.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -15,10 +15,20 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+        registry.addViewController("/main.html").setViewName("dashboard");
+        registry.addViewController("/ll.html").setViewName("list");
     }
 
+    //自定义的国际化组件就生效了
     @Bean
-    public LocaleResolver localeResolver(){
+    public LocaleResolver localeResolver() {
         return new MyLocaleResolver();
+    }
+
+    //拦截器
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginHandlerInterceptor())
+                .addPathPatterns("/**").excludePathPatterns("/index.html", "/", "/user/login", "/css/*", "/js/**", "/img/**");
     }
 }
---
+++ src/main/java/com/huang/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.huang.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+public class LoginController {
+
+
+    @RequestMapping("/user/login")
+    public String login(
+            @RequestParam("username") String username,
+            @RequestParam("password") String password,
+            Model model, HttpSession session) {
+        if (!StringUtils.isEmpty(username) && "admin".equals(password)) {
+            session.setAttribute("loginUser", username);
+            return "redirect:/main.html";
+        } else {
+            //告诉用户，你登录失败了
+            model.addAttribute("msg", "用户名或者密码错误！");
+            return "index";
+        }
+    }
+}
