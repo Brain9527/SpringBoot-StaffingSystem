85e58f54a9dd188be9e4a0adaea4ae2c99af7de4
zero <1580359334@qq.com>
2021-04-20 11:00:33 +0800

    首页实现 国际化 中英切换

--- src/main/java/com/huang/config/MyMvcConfig.java
+++ src/main/java/com/huang/config/MyMvcConfig.java
@@ -1,6 +1,8 @@
 package com.huang.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,5 +15,10 @@ public class MyMvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/index.html").setViewName("index");
+    }
+
+    @Bean
+    public LocaleResolver localeResolver(){
+        return new MyLocaleResolver();
     }
 }
